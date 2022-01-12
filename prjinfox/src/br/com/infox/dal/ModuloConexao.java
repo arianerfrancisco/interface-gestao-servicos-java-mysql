61900e25886efc95422b11c028bf046dc803f308
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-01-12 12:51:24 -0300

    config dal

--- prjinfox/src/br/com/infox/dal/ModuloConexao.java
+++ prjinfox/src/br/com/infox/dal/ModuloConexao.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.infox.dal;
+import java.sql.*;
 
-/**
- *
- * @author Ariane
- */
 public class ModuloConexao {
-    
+    // metodo responsavel por estabelecer a conexao com o banco
+    public  static Connection conector(){
+        java.sql.Connection conexao = null;
+    // a linha abaixo 'chama' o driver
+    String driver="com.mysql.cj.jdbc.Driver";
+    // armazenando informacoes referente ao BD
+    String url ="jdbc:mysql://127.0.0.1:3306/dbinfox";
+    String user="root";
+    String password="123456";
+    // estabelecendo a conexao com o banco
+        try {
+            Class.forName(driver);
+            conexao=DriverManager.getConnection(url,user,password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
